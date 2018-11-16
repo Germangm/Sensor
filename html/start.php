095aa5d83697f15584343cc9342ea55cfeab08cc
Germangm <45084347+Germangm@users.noreply.github.com>
2018-11-15 20:26:12 -0600

    Add files via upload

--- html/start.php
+++ html/start.php
@@ -1,18 +1,18 @@
 <?php
 // Set time.
 exec("sudo date -u -s \"@" . $_POST["time"] . "\"");
-// Make sure device is connected.
+
 $port = exec("ls -ltr /dev|grep -i ttyACM0");
 if ($port != "")
 {
-	// Kill any running data loggers.
+
 	exec("sudo pkill python3");
-	// Start the data logger.
-	exec("sudo python3 /home/pi/data_logger.py > /dev/null &");
-	echo("Started Data Logger");
+
+	exec("sudo python3 /home/pi/recolector.py > /dev/null &");
+	echo("Recolectando Datos");
 }
 else
 {
-	echo("No device connected");
+	echo("Favor de Conectar y prender el Sensor");
 }
 ?>
